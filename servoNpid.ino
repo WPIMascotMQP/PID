a23c58ebdbc20a6d7da5eb94d1b988fd7657d50b
Treksh Marwaha <trekshcool@gmail.com>
2020-01-26 12:54:31 -0500

    Servo and PID
    Threads not supported in Arduino(Threads supported in raspi/ESP32 so shouldn't be a problem in real implementation) but the basic implementation of PID and multiple servo working without threads.
    
    Min/Max done.

---
+++ servoNpid.ino
@@ -0,0 +1,66 @@
+#include <Servo.h>
+#include <PID_v1.h>
+
+Servo s1;
+Servo s2;
+
+#define Kp1 0.7    //proportional coefficient
+#define Ki1 0.2    //integral coefficient
+#define Kd1 0.2    //derivative coefficient
+double Setpoint1, Input1, Output1, Max1, Min1;
+PID myPID1(&Input1, &Output1, &Setpoint1,Kp1,Ki1,Kd1, DIRECT);
+
+
+
+#define Kp2 0.7    //proportional coefficient
+#define Ki2 0.2    //integral coefficient
+#define Kd2 0.2    //derivative coefficient
+double Setpoint2, Input2, Output2, Max2, Min2;
+PID myPID2(&Input2, &Output2, &Setpoint2,Kp2,Ki2,Kd2, DIRECT);
+
+
+int s1Ipin = 0;
+int s2Ipin = 1;  
+
+int s1Opin = 2;
+int s2Opin = 3;  
+
+void setup() {
+  s1.attach(9);
+  s2.attach(10); 
+
+  Setpoint1 = 0;
+  Setpoint2 = 0;
+  Max1 = 180;
+  Min1 = 0;
+  Max2 = 180;
+  Min2 = 0;
+  
+  myPID1.SetOutputLimits(Min1, Max1);
+  myPID2.SetOutputLimits(Min2, Max2);
+  myPID1.SetSampleTime(5);
+  myPID2.SetSampleTime(5);
+
+  Input1 = analogRead(s1Ipin);
+  s1.writeMicroseconds(Setpoint1);
+
+  Input2 = analogRead(s2Ipin);
+  s2.writeMicroseconds(Setpoint2);
+
+  myPID1.SetMode(AUTOMATIC);
+  myPID2.SetMode(AUTOMATIC);
+  
+
+}
+
+void loop() {
+  Input1 = analogRead(s1Ipin);
+  Input2 = analogRead(s2Ipin);
+  Setpoint1 = 100;
+  Setpoint2 = 100;
+  myPID1.Compute();
+  myPID2.Compute();
+  s1.writeMicroseconds(Output1);  
+  s1.writeMicroseconds(Output2);  
+
+}
